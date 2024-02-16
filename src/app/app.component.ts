d9d5ca1087e90a7d41f9a2b788ab0a337094060b
Neha Gholap <nehagholap98@gmail.com>
2024-02-16 16:36:56 +0530

    First modular

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-first></app-first>
+<app-second></app-second>
+<app-third></app-third>
+
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'modular';
+}
---
+++ src/app/first/first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  template: `
+  <h1> Inside First Component </h1>
+  `,
+  styles: []
+})
+export class FirstComponent {
+
+}
